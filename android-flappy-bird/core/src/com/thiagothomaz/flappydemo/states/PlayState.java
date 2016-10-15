807896661f9418bf12ea40e525a7c974d8d9f149
Thiago Thomaz <thiagorthomaz@gmail.com>
2016-10-15 14:01:45 -0300

    Added tube classes

--- android-flappy-bird/core/src/com/thiagothomaz/flappydemo/states/PlayState.java
+++ android-flappy-bird/core/src/com/thiagothomaz/flappydemo/states/PlayState.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.thiagothomaz.flappydemo.FlapplyDemo;
 import com.thiagothomaz.flappydemo.sprites.Bird;
+import com.thiagothomaz.flappydemo.sprites.Tube;
 
 /**
  * Created by thiago on 12/10/16.
@@ -13,12 +14,14 @@ public class PlayState extends State {
 
     private Bird bird;
     private Texture bg;
+    private Tube tube;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
         bird = new Bird(50, 300);
         cam.setToOrtho(false,FlapplyDemo.WIDTH/2, FlapplyDemo.HEIGHT/2);
         bg = new Texture("bg.png");
+        tube = new Tube(100);
 
     }
 
@@ -42,6 +45,8 @@ public class PlayState extends State {
         sb.begin();
         sb.draw(bg, cam.position.x - (cam.viewportWidth/2), 0);
         sb.draw(bird.getBird(), bird.getPosition().x, bird.getPosition().y);
+        sb.draw(tube.getTopTube(), tube.getPosTopTube().x, tube.getPosTopTube().y);
+        sb.draw(tube.getBottomTube(), tube.getPotBotTube().x, tube.getPotBotTube().y);
         sb.end();
     }
 
